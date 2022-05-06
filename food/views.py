319e842f15dca8d79a1a8a789e86b78cbe82f015
christian <christian.ici17@gmail.com>
2022-05-06 16:33:10 -0400

    add insert on get

--- food/views.py
+++ food/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
-
+from food.models import Food
+import json
 
 def index(request):
+    salad = 'salad'
+    entrance = 'entrance'
+    desert = 'desert'
+    user = Food(1, salad, entrance, desert)
+    user.save()
+    items = Food.objects.all()
+    return HttpResponse(items.values())
+
+def post(request):
+    # username = request.POST.get('')
+    salad = 'salad'
+    entrance = 'entrance'
+    desert = 'desert'
+    user = Food(1, salad, entrance, desert)
+    user.save()
     return HttpResponse("Hello, world. You're at the food index.")
