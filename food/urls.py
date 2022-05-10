710b4e3e089df8df3a9805012b33648e82a5873e
christian <christian.ici17@gmail.com>
2022-05-09 22:26:54 -0400

    remove views

--- food/urls.py
+++ food/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from django.conf.urls import url
 from .views import *
 
-from . import views
-
 urlpatterns = [
     # path('', views.index, name='index'),
     # url(r'^/create', createMenu.as_view(), name="create")
